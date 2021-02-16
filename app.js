58d089bee4a89443997734d5401c158d25c445a4
Pierce <piercepatrick2001@gmail.com>
2021-02-16 17:45:03 -0600

    added displayRunningScore function

--- app.js
+++ app.js
@@ -14,48 +14,82 @@ function playRound(playerSelection, computerSelection)
     if (playerSelection == computerSelection) return 'Tie!';
     if (playerSelection == 'rock' && computerSelection == 'paper') {
         computerWins++;
+        displayRunningScore(playerWins, computerWins)
         return 'You Lose! Paper beats Rock';
     }
     if (playerSelection == 'rock' && computerSelection == 'scissors') {
         playerWins++;
+        displayRunningScore(playerWins, computerWins)
         return 'You Win! Rock beats Scissors!';
     }
     if (playerSelection == 'paper' && computerSelection == 'scissors') {
         computerWins++;
+        displayRunningScore(playerWins, computerWins)
         return 'You Lose! Scissors beats Paper';
     }
     if (playerSelection == 'paper' && computerSelection == 'rock') {
         playerWins++;
+        displayRunningScore(playerWins, computerWins)
         return 'You Win! Paper beats Rock';
     }
     if (playerSelection == 'scissors' && computerSelection == 'paper') {
         playerWins++;
+        displayRunningScore(playerWins, computerWins)
         return 'You Win! Scissors beats Paper';
     }
     if (playerSelection == 'scissors' && computerSelection == 'rock') {
         computerWins++;
+        displayRunningScore(playerWins, computerWins)
         return 'You Lose! Rock beats Scissors';
     }
 }
 
 function game()
 {
-    let playerSelection = window.prompt('Rock, Paper, or Scissors?');
-    computerSelection = computerPlay();
-    console.log(playRound(playerSelection, computerSelection));
-    if (playerWins > computerWins) console.log('You win the game!');
-    else if (computerWins > playerWins) console.log('You lose the game!');
-    else console.log('Your game ended with tie!');
+    for (i=0;i<5;i++)
+    {
+        //let playerSelection = window.prompt('Rock, Paper, or Scissors?');
+        computerSelection = computerPlay();
+        console.log(playRound(playerSelection, computerSelection));
+        if (playerWins > computerWins) console.log('You win the game!');
+        else if (computerWins > playerWins) console.log('You lose the game!');
+        else console.log('Your game ended with tie!');
+    }
 }
 
+function displayRunningScore(playerWins, computerWins) {
+    if (playerWins == 5)
+    {
+    document.getElementById('runningScoreText').innerHTML = 'You win the game! Press the play again button to play again.';
+    }
+    if (computerWins == 5)
+    {
+    document.getElementById('runningScoreText').innerHTML = 'The computer wins the game! Press the play again button to play again.';
+    }
+    else
+    {
+        document.getElementById('runningScoreText').innerHTML = 'Your wins: ' + playerWins + 'Computer Wins: ' + computerWins;
+
+    }
+}
+
+
+// declarations for displaying results
 const results = document.querySelector('#results');
 let headingVar = document.createElement('H5');
 headingVar.id = 'headingText';
 results.appendChild(headingVar);
 
+// declarations for displaying running score 
+const runningScore = document.querySelector('#runningscore');
+let runningScoreHeadingVar = document.createElement('H5');
+runningScoreHeadingVar.id = 'runningScoreText';
+runningScore.appendChild(runningScoreHeadingVar);
+
+
+// event listeners
 const rockbtn = document.getElementsByClassName('rock')[0];
 rockbtn.addEventListener( 'click', () => {
-
     playerSelection = 'Rock';
     computerSelection = computerPlay();
     document.getElementById('headingText').innerHTML = playRound(playerSelection, computerSelection);
